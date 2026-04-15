8d5c9d598f28f580368387193a8e18ebb7cbff82
Nur-Sidik <nursidik1500@gmail.com>
2026-04-16 04:14:06 +0700

    Add project files.

---
+++ FormLogin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manajemen_Distribusi_Buah
+{
+    public partial class FormLogin : Form
+    {
+        private readonly SqlConnection conn;
+        private readonly string connectionString = "Data Source=MSI\\UNKNOWNMEMBER;Initial Catalog=DBDistribusiBuah;Integrated Security=True";
+        public FormLogin()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
