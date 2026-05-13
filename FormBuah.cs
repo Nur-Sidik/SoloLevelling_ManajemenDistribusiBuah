095298d4e685afa2e20012d30bc68406566c1c8e
Nur-Sidik <nursidik1500@gmail.com>
2026-05-14 06:18:31 +0700

    UCP2-Menambahkan Binding Source Data

--- FormBuah.cs
+++ FormBuah.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,16 @@ namespace Manajemen_Distribusi_Buah
 {
     public partial class FormBuah : Form
     {
+        private BindingSource bindingSourceBuah = new BindingSource();
+        private DataTable dtBuah = new DataTable();
+        private readonly SqlConnection conn;
+        private readonly string connectionString = "Data Source=MSI\\UNKNOWNMEMBER;Initial Catalog=ManajemenBuah;Integrated Security=True";
+
         public FormBuah()
         {
+            conn = new SqlConnection(connectionString);
             InitializeComponent();
         }
-    }
+
+
 }
